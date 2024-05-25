629a7dc31f7b3df111198e1a3660968f0168c1e0
Antonio Blescia <a.blescia@nocommentlab.it>
2024-05-25 12:48:05 +0200

    fixed wronk set variable calling

--- syslog_parser/utils/GrokFunnel.py
+++ syslog_parser/utils/GrokFunnel.py
@@ -8,16 +8,14 @@ class GrokFunnel:
     @property
     def grok_patterns(self):
         return self._grok_patterns
-    
-    @grok_patterns.setter
-    def __set_grok_patterns(self, value):
-        self._grok_patterns = value
-    
+
+
+
     def load_grok_patterns(self, yaml_file, keyword):
         with open(yaml_file, 'r', encoding='ascii') as file:
             patterns = yaml.safe_load(file)
         if keyword in patterns:
-            self.__set_grok_patterns = patterns[keyword]
+            self._grok_patterns = patterns[keyword]
         else:
             raise ValueError(f"No patterns found for keyword: {keyword}") 
     
